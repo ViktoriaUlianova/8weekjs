10144846009cae6dc4d7c43d9214dcbd75686d4d
Viktoria <viktoria.ulianova@mail.ru>
2021-06-24 22:54:43 +0700

    first lesson almost done

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+// function addErrorClass(elemId) {
+//     let el = document.getElementById(elemId);
+//     el.className = "error-input";
+// }
+
+// function addErrorClassToAllInputs() {
+//     addErrorClass('first-name');
+//     addErrorClass('last-name');
+//     addErrorClass('adress');
+
+// }
+
+// setInterval(addErrorClassToAllInputs, 2000);
+
+
+// fetch("https://api.github.com/ViktoriaUlianova").then(response => console.log(response))
+//     .catch(error => console.log(error));
